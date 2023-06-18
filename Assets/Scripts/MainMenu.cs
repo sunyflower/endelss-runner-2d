9561ae150d8c0d524e51f37428b98912347b800e
pola <polaaleksandrowicz@gmail.com>
2023-06-18 21:32:13 +0200

    Main Menu + Second Level

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    //graj (ładowanie następnej sceny)
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+
+    //wyjście z gry
+    public void QuitGame()
+    {
+        Debug.Log("QUIT");
+        Application.Quit();
+    }
+
+
+    public void SelectLevel1()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+
+    public void SelectLevel2()
+    {
+        SceneManager.LoadScene("Game2");
+    }
+
+
+}
---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+
+
+    void Update()
+    {    //Escape jako przycisk menu pauzy 
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+
+            }
+            else
+            {
+                Pause();
+            }
+
+        }
+    }
+
+
+    //powrót do gry
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+
+    //pauza
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+
+    //quit gry do menu
+    public void QuitGame()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Main Menu");
+    }
+
+}
